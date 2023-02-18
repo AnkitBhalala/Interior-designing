8216c28f2e154ea6adcddfed5eac0a2e76fb596c
shilpi soni <37543434+Shilpi97@users.noreply.github.com>
2023-02-19 03:01:10 +0530

    header logo and css done

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,5 +1,7 @@
 import { Menu } from 'antd';
 import { Link } from "react-router-dom";
+import './Header.css';
+
 
 export function Header() {
   const items = [
@@ -15,8 +17,41 @@ export function Header() {
   ];
   return (
 
-    <div>
-      <Menu defaultSelectedKeys={['home']} mode="horizontal" items={items} />
+    <div className='bordTop bgOrange'>
+      <div className='rgtAln mtop10' >
+       <img width={30} src="instalogo.png" alt="instagram" className='marg20'></img>
+       <img width={30} src="facebooklogo.png" alt='facebook' className='marg20'></img>
+      </div>
+      <div className='dflex fleRow bggrey'>
+      <div className='w33 imgLeft' >
+      <img
+          width={150}
+          height={80}
+          src="2140201.png"
+          alt='logo' 
+          
+        ></img>
+        </div>
+        <div className='w33 dflex' >
+        <img
+          width={450}
+          height={200}
+          src="Bedeck_Black_Rust_Logo.png"
+          alt='logo' 
+          className='mauto'
+        ></img>
+        </div>
+        <div className='w33 imgRgt'>
+        <img
+          width={170}
+          height={75}
+          src="1836813-ff9800.png"
+          alt='logo' 
+          className='floRgt'
+        ></img>
+        </div>
+      </div>
+      <Menu defaultSelectedKeys={['home']} mode="horizontal" items={items} className='w100 menubg' />
     </div>
   );
 }
